5075ce3381d09030eccd8f3ed9cd2daab2b1b1c5
Jessica Gladstone <jcg321@comcast.net>
2018-10-01 20:59:31 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,17 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.each_with_index do |person, index|
+  puts "#{index.next}. #{person}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet (array)
+    arr= array.collect {|item| item.capitalize + "!"}
+    return arr
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls (array)
+    return array.any? {|arr| array.length > 4 }
 end
 
 def find_the_cheese# code an argument here
