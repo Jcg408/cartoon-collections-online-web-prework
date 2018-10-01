9570e81dd4444ca74515087402f816da878df070
Jessica Gladstone <jcg321@comcast.net>
2018-10-01 21:00:45 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,16 +6,16 @@ end
 
 def summon_captain_planet (array)
     arr= array.collect {|item| item.capitalize + "!"}
-    return arr
+     arr
 end
 
 def long_planeteer_calls (array)
-    return array.any? {|arr| array.length > 4 }
+     array.any? {|arr| array.length > 4 }
 end
 
 def find_the_cheese (array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do|item|
-     return cheese_types.include?(item)
+      cheese_types.include?(item)
   end
 end
